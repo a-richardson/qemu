a-richardson/qemu

commit 6b74662bca55fb4a36042e6d758f6000a2fc9f07

riscv: Add mode-dependent jumps and compressed instructions
Whilst here, fix JAL to write back the offset not the address.

target/cheri-common/op_helper_cheri_common.c
@@ -211,23 +211,28 @@ void CHERI_HELPER_IMPL(ccleartag(CPUArchState *env, uint32_t cd, uint32_t cb))
 }
 
 void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd, uint32_t cb,
-                             target_ulong link_pc))
+                             target_ulong offset, target_ulong link_pc))
 {
     /*
      * CJALR: Jump and Link Capability Register
      */
     GET_HOST_RETPC();
     const cap_register_t *cbp = get_readonly_capreg(env, cb);
+    const target_ulong cursor = cap_get_cursor(cbp);
+    const target_ulong addr = cursor + (target_long)offset;
     if (!cbp->cr_tag) {
         raise_cheri_exception(env, CapEx_TagViolation, cb);
-    } else if (cap_is_sealed_with_type(cbp)) {
-        // Note: "sentry" caps can be called using cjalr
+    } else if (cap_is_sealed_with_type(cbp) ||
+               (offset != 0 && !cap_is_unsealed(cbp))) {
+        // Note: "sentry" caps can be called using cjalr, but only if the
+        // immediate offset is 0.
         raise_cheri_exception(env, CapEx_SealViolation, cb);
     } else if (!cap_has_perms(cbp, CAP_PERM_EXECUTE)) {
         raise_cheri_exception(env, CapEx_PermitExecuteViolation, cb);
     } else if (!cap_has_perms(cbp, CAP_PERM_GLOBAL)) {
         raise_cheri_exception(env, CapEx_GlobalViolation, cb);
-    } else if (!validate_jump_target(env, cbp, cb, _host_return_address)) {
+    } else if (!validate_jump_target(env, cbp, addr, cb,
+                                     _host_return_address)) {
         assert(false && "Should have raised an exception");
     }
 
@@ -236,6 +241,14 @@ void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd, uint32_t cb,
     if (cap_is_sealed_entry(cbp)) {
         // If we are calling a "sentry" cap, remove the sealed flag
         cap_unseal_entry(&next_pcc);
+        assert(cap_get_cursor(&next_pcc) == addr &&
+               "Should have raised an exception");
+    } else {
+        // Can never create an unrepresentable capability since we
+        // bounds-checked the jump target.
+        assert(is_representable_cap_with_addr(&next_pcc, addr) &&
+               "Target addr must be representable");
+        next_pcc._cr_cursor = addr;
     }
     // Don't generate a link capability if cd == zero register
     if (cd != 0) {
@@ -243,11 +256,9 @@ void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd, uint32_t cb,
         // since we are writing a new cursor anyway.
         cap_register_t result = *cheri_get_recent_pcc(env);
         // can never create an unrepresentable capability since PCC must be in bounds
-        result._cr_cursor = link_pc;
-#if QEMU_USE_COMPRESSED_CHERI_CAPS == 1
-        assert(cc128_is_representable_with_addr(&result, link_pc) &&
+        assert(is_representable_cap_with_addr(&result, link_pc) &&
                "Link addr must be representable");
-#endif
+        result._cr_cursor = link_pc;
         // The return capability should always be a sentry
         cap_make_sealed_entry(&result);
         update_capreg(env, cd, &result);
@@ -282,7 +293,8 @@ void CHERI_HELPER_IMPL(cinvoke(CPUArchState *env, uint32_t code_regnum,
         raise_cheri_exception(env, CapEx_PermitExecuteViolation, code_regnum);
     } else if (cap_has_perms(data_cap, CAP_PERM_EXECUTE)) {
         raise_cheri_exception(env, CapEx_PermitExecuteViolation, data_regnum);
-    } else if (!validate_jump_target(env, code_cap, code_regnum, _host_return_address)) {
+    } else if (!validate_jump_target(env, code_cap, cap_get_cursor(code_cap),
+                                     code_regnum, _host_return_address)) {
         raise_cheri_exception(env, CapEx_LengthViolation, code_regnum);
     } else {
         // Unseal code and data cap now that the checks have succeeded.

target/riscv/insn_trans/trans_cheri.c.inc
@@ -250,12 +250,36 @@ static bool trans_csetboundsimm(DisasContext *ctx, arg_cincoffsetimm *a)
 }
 
 /// Control-flow instructions
-static bool trans_cjalr(DisasContext *ctx, arg_cjalr *a)
+static void gen_cjal(DisasContext *ctx, int rd, target_ulong imm)
 {
-    TCGv_i32 dest_regnum = tcg_const_i32(a->rd);
-    TCGv_i32 source_regnum = tcg_const_i32(a->rs1);
+    target_ulong next_pc;
+
+    /* check misaligned: */
+    next_pc = ctx->base.pc_next + imm;
+    if (!has_ext(ctx, RVC)) {
+        if ((next_pc & 0x3) != 0) {
+            gen_exception_inst_addr_mis(ctx);
+            return;
+        }
+    }
+    TCGv_i32 dst = tcg_const_i32(rd);
+    TCGv new_cursor = tcg_const_tl(ctx->pc_succ_insn);
+    gen_helper_auipcc(cpu_env, dst, new_cursor);
+    tcg_temp_free(new_cursor);
+    tcg_temp_free_i32(dst);
+
+    gen_goto_tb(ctx, 0, ctx->base.pc_next + imm, /*bounds_check=*/true); /* must use this for safety */
+    ctx->base.is_jmp = DISAS_NORETURN;
+}
+
+static void gen_cjalr(DisasContext *ctx, int rd, int rs1, target_ulong imm)
+{
+    TCGv_i32 dest_regnum = tcg_const_i32(rd);
+    TCGv_i32 source_regnum = tcg_const_i32(rs1);
+    TCGv imm_value = tcg_const_tl(imm);
     TCGv t0 = tcg_const_tl(ctx->pc_succ_insn); // Link addr + resulting $pc
-    gen_helper_cjalr(cpu_env, dest_regnum, source_regnum, t0);
+    gen_helper_cjalr(cpu_env, dest_regnum, source_regnum, imm_value, t0);
+    tcg_temp_free(imm_value);
     tcg_temp_free(t0);
     tcg_temp_free_i32(source_regnum);
     tcg_temp_free_i32(dest_regnum);
@@ -263,6 +287,17 @@ static bool trans_cjalr(DisasContext *ctx, arg_cjalr *a)
     lookup_and_goto_ptr(ctx);
     // PC has been updated -> exit translation block
     ctx->base.is_jmp = DISAS_NORETURN;
+}
+
+static bool trans_jalr_cap(DisasContext *ctx, arg_jalr_cap *a)
+{
+    gen_cjalr(ctx, a->rd, a->rs1, 0);
+    return true;
+}
+
+static bool trans_jalr_pcc(DisasContext *ctx, arg_jalr_pcc *a)
+{
+    gen_jalr(ctx, a->rd, a->rs1, 0);
     return true;
 }
 

target/cheri-common/cheri-helper-common.h
@@ -87,7 +87,7 @@ DEF_HELPER_4(candaddr, void, env, i32, i32, tl)
 DEF_HELPER_4(candperm, void, env, i32, i32, tl)
 DEF_HELPER_4(cfromptr, void, env, i32, i32, tl)
 DEF_HELPER_4(cincoffset, void, env, i32, i32, tl)
-DEF_HELPER_4(cjalr, void, env, i32, i32, tl)
+DEF_HELPER_5(cjalr, void, env, i32, i32, tl, tl)
 DEF_HELPER_4(csetaddr, void, env, i32, i32, tl)
 DEF_HELPER_4(csetbounds, void, env, i32, i32, tl)
 DEF_HELPER_4(csetboundsexact, void, env, i32, i32, tl)

target/mips/cheri-archspecific.h
@@ -117,17 +117,16 @@ static inline void QEMU_NORETURN raise_unaligned_store_exception(
 }
 
 static inline bool validate_jump_target(CPUMIPSState *env,
-                                        const cap_register_t *target,
+                                        const cap_register_t *cap,
+                                        target_ulong addr,
                                         unsigned regnum, uintptr_t retpc)
 {
-    target_ulong target_addr = cap_get_cursor(target);
-    if (!cap_is_in_bounds(target, target_addr, 4)) {
+    if (!cap_is_in_bounds(cap, addr, 4)) {
         raise_cheri_exception_impl(env, CapEx_LengthViolation, regnum, true,
                                    retpc);
     }
-    if (!QEMU_IS_ALIGNED(cap_get_cursor(target), 4)) {
-        do_raise_c0_exception_impl(env, EXCP_AdEL, cap_get_cursor(target),
-                                   retpc);
+    if (!QEMU_IS_ALIGNED(addr, 4)) {
+        do_raise_c0_exception_impl(env, EXCP_AdEL, addr, retpc);
     }
     return true;
 }

target/mips/translate_cheri.c
@@ -190,9 +190,10 @@ static inline void generate_cjalr(DisasContext *ctx, int32_t cd, int32_t cb)
     } else {
         TCGv_i32 tcd = tcg_const_i32(cd);
         TCGv_i32 tcb = tcg_const_i32(cb);
+        TCGv toff = tcg_const_tl(0);
         /* Instruction size is always four bytes and so is the delay slot */
         TCGv link_addr = tcg_const_tl(ctx->base.pc_next + 8);
-        gen_helper_cjalr(cpu_env, tcd, tcb, link_addr);
+        gen_helper_cjalr(cpu_env, tcd, tcb, toff, link_addr);
         /* Set branch and delay slot flags */
         ctx->hflags |= (MIPS_HFLAG_BRC | MIPS_HFLAG_BDS32);
         /* Save capability register index that is new PCC */
@@ -200,6 +201,7 @@ static inline void generate_cjalr(DisasContext *ctx, int32_t cd, int32_t cb)
         save_cpu_state(ctx, 0);
 
         tcg_temp_free(link_addr);
+        tcg_temp_free(toff);
         tcg_temp_free_i32(tcb);
         tcg_temp_free_i32(tcd);
     }

target/riscv/cheri-archspecific.h
@@ -80,23 +80,23 @@ static inline void QEMU_NORETURN raise_unaligned_store_exception(
 }
 
 static inline bool validate_jump_target(CPUArchState *env,
-                                        const cap_register_t *target,
+                                        const cap_register_t *cap,
+                                        target_ulong addr,
                                         unsigned regnum, uintptr_t retpc)
 {
-    target_ulong new_base = cap_get_base(target);
-    target_ulong new_addr = cap_get_cursor(target);
+    target_ulong base = cap_get_base(cap);
     unsigned min_insn_size = riscv_has_ext(env, RVC) ? 2 : 4;
-    if (!cap_is_in_bounds(target, new_addr, min_insn_size)) {
+    if (!cap_is_in_bounds(cap, addr, min_insn_size)) {
         raise_cheri_exception_impl(env, CapEx_LengthViolation, regnum, true,
                                    retpc);
     }
-    if (!QEMU_IS_ALIGNED(new_base, min_insn_size)) {
+    if (!QEMU_IS_ALIGNED(base, min_insn_size)) {
         raise_cheri_exception_impl(env, CapEx_UnalignedBase, regnum, true,
                                    retpc);
     }
     // XXX: Sail only checks bit 1 why not also bit zero? Is it because that is
     // ignored?
-    if (!riscv_has_ext(env, RVC) && (new_addr & 0x2)) {
+    if (!riscv_has_ext(env, RVC) && (addr & 0x2)) {
         riscv_raise_exception(env, RISCV_EXCP_INST_ADDR_MIS, retpc);
     }
     return true;

target/riscv/insn16-32.decode
@@ -17,12 +17,29 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # *** RV32C Standard Extension (Quadrant 0) ***
-flw               011  ... ... .. ... 00 @cl_w
-fsw               111  ... ... .. ... 00 @cs_w
+fld               001  ... ... .. ... 00 @cl_d
+{
+  flw             011  ... ... .. ... 00 @cl_w ?!pred_capmode
+  lc              011  ... ... .. ... 00 @cl_d ?pred_capmode
+}
+fsd               101  ... ... .. ... 00 @cs_d
+{
+  fsw             111  ... ... .. ... 00 @cs_w ?!pred_capmode
+  sc              111  ... ... .. ... 00 @cs_d ?pred_capmode
+}
 
 # *** RV32C Standard Extension (Quadrant 1) ***
 jal               001     ........... 01 @cj    rd=1  # C.JAL
 
 # *** RV32C Standard Extension (Quadrant 2) ***
-flw               011 .  .....  ..... 10 @c_lwsp
-fsw               111 .  .....  ..... 10 @c_swsp
+fld               001 .  .....  ..... 10 @c_ldsp
+{
+  flw             011 .  .....  ..... 10 @c_lwsp ?!pred_capmode
+  illegal         011 -  00000  ----- 10         ?pred_capmode # RES rd=0
+  lc              011 .  .....  ..... 10 @c_ldsp ?pred_capmode
+}
+fsd               101   ......  ..... 10 @c_sdsp
+{
+  fsw             111 .  .....  ..... 10 @c_swsp ?!pred_capmode
+  sc              111 .  .....  ..... 10 @c_sdsp ?pred_capmode
+}

target/riscv/insn16-64.decode
@@ -16,8 +16,29 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Immediates:
+%uimm_cl_q     10:1 5:2 11:2      !function=ex_shift_4
+
+%uimm_6bit_lq 2:4 12:1 6:1           !function=ex_shift_4
+%uimm_6bit_sq 7:4 11:2               !function=ex_shift_4
+
+# Formats 16:
+@cl_q      ... ... ... .. ... .. &i      imm=%uimm_cl_q   rs1=%rs1_3  rd=%rs2_3
+@cs_q      ... ... ... .. ... .. &s      imm=%uimm_cl_q   rs1=%rs1_3  rs2=%rs2_3
+
+@c_lqsp    ... . .....  ..... .. &i      imm=%uimm_6bit_lq rs1=2 %rd
+@c_sqsp    ... . .....  ..... .. &s      imm=%uimm_6bit_sq rs1=2 rs2=%rs2_5
+
 # *** RV64C Standard Extension (Quadrant 0) ***
+{
+  fld             001  ... ... .. ... 00 @cl_d ?!pred_capmode
+  lc              001  ... ... .. ... 00 @cl_q ?pred_capmode
+}
 ld                011  ... ... .. ... 00 @cl_d
+{
+  fsd             101  ... ... .. ... 00 @cs_d ?!pred_capmode
+  sc              101  ... ... .. ... 00 @cs_q ?pred_capmode
+}
 sd                111  ... ... .. ... 00 @cs_d
 
 # *** RV64C Standard Extension (Quadrant 1) ***
@@ -30,7 +51,16 @@ addw              100 1 11 ... 01 ... 01 @cs_2
 
 # *** RV64C Standard Extension (Quadrant 2) ***
 {
+  fld             001 .  .....  ..... 10 @c_ldsp ?!pred_capmode
+  illegal         001 -  00000  ----- 10         ?pred_capmode # RES rd=0
+  lc              001 .  .....  ..... 10 @c_lqsp ?pred_capmode
+}
+{
   illegal         011 -  00000  ----- 10 # c.ldsp, RES rd=0
   ld              011 .  .....  ..... 10 @c_ldsp
+}
+{
+  fsd             101   ......  ..... 10 @c_sdsp ?!pred_capmode
+  sc              101   ......  ..... 10 @c_sqsp ?pred_capmode
 }
 sd                111 .  .....  ..... 10 @c_sdsp

commit de35878383915ffbc576631bf7a544e6d6ad6652

[CHERI-RISC-V] Write sentries to the link register for CJAL
The existing implementation just called auipcc which gives an unsealed
value. This adds a new cheri_jump_and_link_checked() helper function that
implementations the shared parts of cjal and cjalr.

Fixes: 6b74662bca55fb4a36042e6d758f6000a2fc9f07

target/cheri-common/cheri-helper-utils.h
@@ -385,6 +385,12 @@ cap_register_t load_and_decompress_cap_from_memory_raw(
 void cheri_jump_and_link(CPUArchState *env, const cap_register_t *target,
                          target_ulong addr, uint32_t link_reg,
                          target_ulong link_pc, uint32_t cjalr_flags);
+void cheri_jump_and_link_checked(CPUArchState *env, uint32_t link_reg,
+                                 target_ulong link_pc, uint32_t target_reg,
+                                 const cap_register_t *target,
+                                 target_ulong target_addr,
+                                 target_ulong immediate_offset, uint32_t flags,
+                                 uintptr_t _host_return_address);
 
 void squash_mutable_permissions(CPUArchState *env, target_ulong *pesbt,
                                 const cap_register_t *source);

target/cheri-common/op_helper_cheri_common.c
@@ -410,6 +410,38 @@ void cheri_jump_and_link(CPUArchState *env, const cap_register_t *target,
     update_next_pcc_for_tcg(env, &next_pcc, cjalr_flags);
 }
 
+void cheri_jump_and_link_checked(CPUArchState *env, uint32_t link_reg,
+                                 target_ulong link_pc, uint32_t target_reg,
+                                 const cap_register_t *target,
+                                 target_ulong target_addr,
+                                 target_ulong immediate_offset, uint32_t flags,
+                                 uintptr_t _host_return_address)
+{
+/* Morello takes the exception at the target. */
+#if !CHERI_CONTROLFLOW_CHECK_AT_TARGET
+    if (!target->cr_tag) {
+        raise_cheri_exception(env, CapEx_TagViolation, target_reg);
+    } else if (cap_is_sealed_with_type(target) ||
+               (immediate_offset != 0 && !cap_is_unsealed(target))) {
+        // Note: "sentry" caps can be called using cjalr, but only if the
+        // immediate offset is 0.
+        raise_cheri_exception(env, CapEx_SealViolation, target_reg);
+    } else if (!cap_has_perms(target, CAP_PERM_EXECUTE)) {
+        raise_cheri_exception(env, CapEx_PermitExecuteViolation, target_reg);
+    } else if (!cap_has_perms(target, CAP_PERM_GLOBAL)) {
+        raise_cheri_exception(env, CapEx_GlobalViolation, target_reg);
+    } else if (!validate_jump_target(env, target, target_addr, target_reg,
+                                     _host_return_address)) {
+        assert(false && "Should have raised an exception");
+    }
+#endif
+#ifdef TARGET_RISCV
+    /* On RISC-V we mask the LSB of the target to match JALR behaviour. */
+    target_addr &= ~(target_ulong)1;
+#endif
+    cheri_jump_and_link(env, target, target_addr, link_reg, link_pc, flags);
+}
+
 void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd,
                              uint32_t cb_with_flags, target_ulong offset,
                              target_ulong link_pc))
@@ -429,27 +461,8 @@ void CHERI_HELPER_IMPL(cjalr(CPUArchState *env, uint32_t cd,
     const target_ulong addr = cursor + (target_long)offset;
 #endif
 
-    /* Morello takes the exception at the target. */
-#if !CHERI_CONTROLFLOW_CHECK_AT_TARGET
-    GET_HOST_RETPC();
-    if (!cbp->cr_tag) {
-        raise_cheri_exception(env, CapEx_TagViolation, cb);
-    } else if (cap_is_sealed_with_type(cbp) ||
-               (offset != 0 && !cap_is_unsealed(cbp))) {
-        // Note: "sentry" caps can be called using cjalr, but only if the
-        // immediate offset is 0.
-        raise_cheri_exception(env, CapEx_SealViolation, cb);
-    } else if (!cap_has_perms(cbp, CAP_PERM_EXECUTE)) {
-        raise_cheri_exception(env, CapEx_PermitExecuteViolation, cb);
-    } else if (!cap_has_perms(cbp, CAP_PERM_GLOBAL)) {
-        raise_cheri_exception(env, CapEx_GlobalViolation, cb);
-    } else if (!validate_jump_target(env, cbp, addr, cb,
-                                     _host_return_address)) {
-        assert(false && "Should have raised an exception");
-    }
-#endif
-
-    cheri_jump_and_link(env, cbp, addr, cd, link_pc, cjalr_flags);
+    cheri_jump_and_link_checked(env, cd, link_pc, cb, cbp, addr, offset,
+                                cjalr_flags, GETPC());
 }
 
 void CHERI_HELPER_IMPL(cinvoke(CPUArchState *env, uint32_t code_regnum,

target/riscv/helper.h
@@ -70,6 +70,7 @@ DEF_HELPER_FLAGS_1(fclass_d, TCG_CALL_NO_RWG_SE, tl, i64)
 #include "cheri-helper-common.h"
 DEF_HELPER_4(cspecialrw, void, env, i32, i32, i32)
 DEF_HELPER_3(auipcc, void, env, i32, tl)
+DEF_HELPER_5(cjal, void, env, i32, tl, tl, tl)
 DEF_HELPER_4(amoswap_cap, void, env, i32, i32, i32)
 DEF_HELPER_3(lr_c_modedep, void, env, i32, i32)
 DEF_HELPER_3(lr_c_ddc, void, env, i32, i32)

target/riscv/insn_trans/trans_cheri.c.inc
@@ -252,23 +252,24 @@ static bool trans_csetboundsimm(DisasContext *ctx, arg_cincoffsetimm *a)
 /// Control-flow instructions
 static void gen_cjal(DisasContext *ctx, int rd, target_ulong imm)
 {
-    target_ulong next_pc;
+    target_ulong next_pc = ctx->base.pc_next + imm;
 
-    /* check misaligned: */
-    next_pc = ctx->base.pc_next + imm;
-    if (!has_ext(ctx, RVC)) {
-        if ((next_pc & 0x3) != 0) {
-            gen_exception_inst_addr_mis(ctx);
-            return;
-        }
-    }
     TCGv_i32 dst = tcg_const_i32(rd);
-    TCGv new_cursor = tcg_const_tl(ctx->pc_succ_insn);
-    gen_helper_auipcc(cpu_env, dst, new_cursor);
-    tcg_temp_free(new_cursor);
+    TCGv target_addr = tcg_const_tl(next_pc);
+    TCGv immv = tcg_const_tl(imm);
+    TCGv link_addr = tcg_const_tl(ctx->pc_succ_insn);
+    /*
+     * helper_cjal() checks for misalignment and capability bounds. Having
+     * immediate as a separate argument allows skipping gen_update_cpu_pc().
+     */
+    gen_helper_cjal(cpu_env, dst, target_addr, immv, link_addr);
+    tcg_temp_free(link_addr);
+    tcg_temp_free(immv);
+    tcg_temp_free(target_addr);
     tcg_temp_free_i32(dst);
 
-    gen_goto_tb(ctx, 0, ctx->base.pc_next + imm, /*bounds_check=*/true); /* must use this for safety */
+    /* No bounds check needed here since we did it in helper_cjal(). */
+    gen_goto_tb(ctx, 0, ctx->base.pc_next + imm, false);
     ctx->base.is_jmp = DISAS_NORETURN;
 }
 

target/riscv/op_helper_cheri.c
@@ -236,6 +236,14 @@ void HELPER(auipcc)(CPUArchState *env, uint32_t cd, target_ulong new_cursor)
     derive_cap_from_pcc(env, cd, new_cursor, GETPC(), OOB_INFO(auipcc));
 }
 
+void HELPER(cjal)(CPUArchState *env, uint32_t cd, target_ulong target_addr,
+                  target_ulong immediate, target_ulong link_addr)
+{
+    cheri_jump_and_link_checked(env, cd, link_addr, CHERI_EXC_REGNUM_PCC,
+                                cheri_get_recent_pcc(env), target_addr,
+                                immediate, 0, GETPC());
+}
+
 void HELPER(amoswap_cap)(CPUArchState *env, uint32_t dest_reg,
                          uint32_t addr_reg, uint32_t val_reg)
 {