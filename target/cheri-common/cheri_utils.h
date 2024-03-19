4f88f3838255990996118ad1c57a12453beb6b52
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-19 10:41:54 +0000

    cheri-common: squelch type limits warnings for objtype
    Silence the type limit warnings ("comparison is always true") for the
    objtype. Objtype was removed in cheri bakewell, the code to handle
    objtype will be removed later as we refactor the internal storage of
    capability permissions.
    
    Use gcc pragma settings to disable -Wtype-limits temporarily. Both gcc
    and clang understand these pragmas.

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -135,8 +135,11 @@ static inline bool cap_otype_is_reserved(target_ulong otype)
 {
     cheri_debug_assert(otype <= CAP_MAX_REPRESENTABLE_OTYPE &&
                        "Should only be called for in-range otypes!");
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits"
     return otype >= CAP_CC(MIN_RESERVED_OTYPE) &&
            otype <= CAP_CC(MAX_RESERVED_OTYPE);
+#pragma GCC diagnostic pop
 }
 
 static inline target_ulong cap_get_otype_unsigned(const cap_register_t *c)
