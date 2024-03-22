0fc8039833e1acaca7fa39cceef0a4c9c4c2c542
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-22 11:25:21 +0000

    cheri: riscv: remove cgethigh for risc-v
    Remove the cgethigh instruction for risc-v. It was replaced with gchi in
    cheri bakewell.
    
    Keep cgethigh for mips (morello has no cgethigh). Move some declarations
    for cgethigh from cheri-common to mips. Keeping the helper declaration in
    cheri-common would require a helper implementation in risc-v, we don't
    want this.

--- disas/riscv.c
+++ disas/riscv.c
@@ -555,7 +555,6 @@ typedef enum {
     rv_op_cmove,
     rv_op_ccleartag,
     rv_op_cjalr,
-    rv_op_cgethigh,
     rv_op_cgetaddr,
     rv_op_csealentry,
     rv_op_cloadtags,
@@ -1319,7 +1318,6 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_ccleartag] = { "ccleartag", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cjalr] = { "cjalr", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cgetaddr] = { "cgetaddr", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
-    [rv_op_cgethigh] = { "cgetaddr", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_csealentry] = { "csealentry", rv_codec_r, rv_fmt_cd_cs1, NULL, 0, 0, 0 },
     [rv_op_cloadtags] = { "cloadtags", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
 
@@ -1594,7 +1592,6 @@ static rv_opcode decode_cheri_two_op(unsigned func) {
     case 0b01111: return rv_op_cgetaddr;
     case 0b10001: return rv_op_csealentry;
     case 0b10010: return rv_op_cloadtags;
-    case 0b10111: return rv_op_cgethigh;
     default: return rv_op_illegal;
     }
 }
