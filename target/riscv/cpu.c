f6b7d2c004702ce43b9334e96ebdfc029f174de8
Paul Buxton <paul.buxton@codasip.com>
2024-04-15 11:47:13 +0100

    Add debug scratch Capability registers.
    As the debug scratch registers do not exist in the cheri qemu branch this adds new registers. At the moment only accessible as capabilities. Should review how these are handled in upstream qemu, and address when we look at XLEN writes / legacy mode.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -761,6 +761,8 @@ static void riscv_cpu_reset(DeviceState *dev)
     null_capability(&env->mscratchc);
     set_max_perms_capability(&env->MTVECC,0);
     set_max_perms_capability(&env->mepcc, 0);
+    null_capability(&env->dscratch0c);
+    null_capability(&env->dscratch1c);
 #endif /* TARGET_CHERI */
 #ifdef CONFIG_DEBUG_TCG
     env->_pc_is_current = true;
