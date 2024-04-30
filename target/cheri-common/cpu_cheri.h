d489de739d2aee1f872b4679d88bcb0ab8da34ef
Martin Kaiser <martin.kaiser@codasip.com>
2024-04-30 17:44:36 +0200

    Revert "cheri: prepare cap_has_capmode_flag for flags removal"
    Revert modifications to the capability format as per our overall
    strategy (Cambdrige github is our upstream, we don't touch mips and
    morello, m&m will continue using the v9 capability format).
    
    This reverts commit c52e44dd030790cab55a733bf52c7c03c027cdb4.

--- target/cheri-common/cpu_cheri.h
+++ target/cheri-common/cpu_cheri.h
@@ -99,14 +99,7 @@ static inline void cheri_update_pcc(cap_register_t *pcc, target_ulong pc_addr,
 }
 
 static inline bool cap_has_capmode_flag(const cap_register_t *cap) {
-    /*
-     * TODO: Implement this using bakewell's mode bit.
-     *
-     * For now, we default to capmode == false. If capmode was enabled, all
-     * addresses would have to be capabilities. This requires changes to
-     * qmemu's hard-wired rom code.
-     */
-    return false;
+    return (cap_get_flags(cap) & CHERI_FLAG_CAPMODE) == CHERI_FLAG_CAPMODE;
 }
 
 static inline bool cheri_in_capmode(CPUArchState *env) {
