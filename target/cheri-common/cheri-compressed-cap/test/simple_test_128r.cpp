40615c7c82341d96f68adffc78fa9097794eac25
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-07 15:47:19 +0200

    cheri-compressed-cap: test some AP encodings for MXLEN=64
    For MXLEN=64, the AP field in the capability is an identical copy of the
    cr_arch_perm variable. Add some test cases for this trivial operation.

--- target/cheri-common/cheri-compressed-cap/test/simple_test_128r.cpp
+++ target/cheri-common/cheri-compressed-cap/test/simple_test_128r.cpp
@@ -3,6 +3,7 @@
 #define TEST_CC_FORMAT_UPPER 128R
 
 #include "test_common.cpp"
+#include "cap_ap.h"
 
 TEST_CASE("update sealed", "[sealed]") {
     _cc_cap_t cap;
@@ -10,3 +11,27 @@ TEST_CASE("update sealed", "[sealed]") {
     _cc_N(update_sealed)(&cap, 1);
     CHECK_FIELD(cap, is_sealed, 1);
 }
+
+/*
+ * For 64-bit bakewell, the conversion between AP bitfield and its encoding
+ * is a trivial 1:1 mapping. Some spot checks will do, there's no point in
+ * checking all combinations.
+ */
+
+/* AP compression */
+
+TEST_CASE_AP_COMP(CAP_AP_R, CAP_AP_R)
+
+TEST_CASE_AP_COMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
+
+/* AP decompression */
+
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
+
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W))
