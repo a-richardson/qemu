206d946d94b717605e588d543fe2a5bca8ba25e4
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-07 15:44:08 +0200

    cheri-compressed-cap: test AP encodings for MXLEN=32
    The capability struct stores the permissions internally as a bitfield in
    the cr_arch_perm component. Test the conversion from cr_arch_perm into
    the AP (architectural permissions) field and vice versa for MXLEN=32.

--- target/cheri-common/cheri-compressed-cap/test/simple_test_64r.cpp
+++ target/cheri-common/cheri-compressed-cap/test/simple_test_64r.cpp
@@ -3,6 +3,7 @@
 #define TEST_CC_FORMAT_UPPER 64R
 
 #include "test_common.cpp"
+#include "cap_ap.h"
 
 TEST_CASE("update sealed", "[sealed]") {
     _cc_cap_t cap;
@@ -10,3 +11,90 @@ TEST_CASE("update sealed", "[sealed]") {
     _cc_N(update_sealed)(&cap, 1);
     CHECK_FIELD(cap, is_sealed, 1);
 }
+
+/*
+ * AP compression
+ */
+
+TEST_CASE_AP_COMP(CAP_AP_R, (CAP_AP_Q0 | 1))
+TEST_CASE_AP_COMP(CAP_AP_W, (CAP_AP_Q0 | 4))
+TEST_CASE_AP_COMP(CAP_AP_R | CAP_AP_W, (CAP_AP_Q0 | 5))
+
+TEST_CASE_AP_COMP(
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR),
+        (CAP_AP_Q1 | 0))
+TEST_CASE_AP_COMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR),
+        (CAP_AP_Q1 | 1))
+TEST_CASE_AP_COMP(
+        (CAP_AP_X | CAP_AP_R | CAP_AP_C),
+        (CAP_AP_Q1 | 2))
+TEST_CASE_AP_COMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_C),
+        (CAP_AP_Q1 | 3))
+TEST_CASE_AP_COMP(
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C),
+        (CAP_AP_Q1 | 4))
+TEST_CASE_AP_COMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C),
+        (CAP_AP_Q1 | 5))
+TEST_CASE_AP_COMP(
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W),
+        (CAP_AP_Q1 | 6))
+TEST_CASE_AP_COMP(
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W),
+        (CAP_AP_Q1 | 7))
+
+TEST_CASE_AP_COMP(CAP_AP_C | CAP_AP_R, (CAP_AP_Q3 | 3))
+TEST_CASE_AP_COMP(CAP_AP_C | CAP_AP_R | CAP_AP_W, (CAP_AP_Q3 | 7))
+
+/* Invalid permissions must result in "no permissions". */
+TEST_CASE_AP_COMP(CAP_AP_C, 0);
+
+/*
+ * AP decompression
+ */
+
+TEST_CASE_AP_DECOMP(CAP_AP_Q0 | 1, CAP_AP_R)
+TEST_CASE_AP_DECOMP(CAP_AP_Q0 | 4, CAP_AP_W)
+TEST_CASE_AP_DECOMP(CAP_AP_Q0 | 5, (CAP_AP_R | CAP_AP_W))
+/* invalid in Q0 -> no permissions */
+TEST_CASE_AP_DECOMP(CAP_AP_Q0 | 6, 0)
+
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 0),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 1),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 2),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_C))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 3),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_C))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 4),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 5),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 6),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W))
+TEST_CASE_AP_DECOMP(
+        (CAP_AP_Q1 | 7),
+        (CAP_AP_M | CAP_AP_X | CAP_AP_R | CAP_AP_W))
+
+/*
+ * invalid in Q2 -> all permissions (infinite capability does not have the
+ * M-bit set)
+ */
+TEST_CASE_AP_DECOMP((CAP_AP_Q2 | 2),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
+
+TEST_CASE_AP_DECOMP((CAP_AP_Q3 | 3), CAP_AP_C | CAP_AP_R)
+TEST_CASE_AP_DECOMP((CAP_AP_Q3 | 7), CAP_AP_C | CAP_AP_R | CAP_AP_W)
+/* invalid in Q3 -> all permissions (no M-bit, see above) */
+TEST_CASE_AP_DECOMP((CAP_AP_Q3 | 1),
+        (CAP_AP_X | CAP_AP_R | CAP_AP_W | CAP_AP_C | CAP_AP_ASR))
