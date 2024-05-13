30291640d5a10270c39ca7fef8b8fc6a44832475
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-13 15:03:37 +0200

    cheri-compressed-cap: fix up asserts about exponent length
    Fix up asserts that check the length of some exponent fields. For cheri
    bakewell with MXLEN=32, both TE and BE are 2 bytes long (and the exponent
    is calculated from T8, TE and BE).
    
    While at it, move the asserts before the comment about exact bounds. This
    comment refers to the "exact" variable, not to the asserts.

--- target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
+++ target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
@@ -1002,13 +1002,18 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_length_t req_len, _
     if (req_base < cap->cr_base || req_top > cap->_cr_top) {
         cap->cr_tag = 0;
     }
+#if _CC_N(FIELD_T8_USED) == 1
+    _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 2, "expected 2 bits to be used by");
+    _CC_STATIC_ASSERT(_CC_EXP_HIGH_WIDTH == 2, "expected 2 bits to be used by");
+#else
+    _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 3, "expected 3 bits to be used by");
+    _CC_STATIC_ASSERT(_CC_EXP_HIGH_WIDTH == 3, "expected 3 bits to be used by");
+#endif
     /*
      * With compressed capabilities we may need to increase the range of
      * memory addresses to be wider than requested so it is
      * representable.
      */
-    _CC_STATIC_ASSERT(_CC_EXP_LOW_WIDTH == 3, "expected 3 bits to be used by");  // expected 3 bits to
-    _CC_STATIC_ASSERT(_CC_EXP_HIGH_WIDTH == 3, "expected 3 bits to be used by"); // expected 3 bits to
     bool exact = false;
     uint32_t new_ebt = _cc_N(compute_ebt)(req_base, req_top, alignment_mask, &exact);
     _cc_addr_t new_base;
