8767aa12d81b64197ce728c8f7a5e9fd8e9e4e06
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-28 11:28:55 +0200

    cheri: riscv: clear the tag for invalid capability
    Clear the tag when a capability becomes invalid, do not throw an
    exception.
    
    This is the required behaviour as per the cheri bakewell specification.
    Configure this unconditionally instead of deriving it from the cheri spec
    version. We're only supporting bakewell for risc-v anyway.
    
    Throwing an exception immediately is a useful feature for debugging, we
    could add a property to enable this from the qemu command line.

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -98,8 +98,7 @@ enum CheriSCR {
 #define CHERI_EXC_REGNUM_PCC (32 + CheriSCR_PCC)
 #define CHERI_EXC_REGNUM_DDC (32 + CheriSCR_DDC)
 #define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
-/* TODO: switch tag clearing to true once CheriBSD is ready for it. */
-#define CHERI_TAG_CLEAR_ON_INVALID(env) (env_archcpu(env)->cfg.ext_cheri_v9)
+#define CHERI_TAG_CLEAR_ON_INVALID(env) true
 #define CHERI_NO_RELOCATION(env)            (env_archcpu(env)->cfg.ext_cheri_v9)
 #define CHERI_TRANSLATE_DDC_RELOCATION(ctx) (!(ctx)->cheri_v9_semantics)
 #define CHERI_TRANSLATE_PCC_RELOCATION(ctx) (!(ctx)->cheri_v9_semantics)
