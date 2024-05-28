6b685321deb45acd734ad00a7127ce0029ed2bce
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-28 10:37:59 +0200

    cheri: riscv: bakewell does not use ddc or pcc relocation
    Disable ddc and pcc relocation. This seems to be an ancient feature that
    was already gone in v9. We're only supporting bakewell, we can disable
    relocation unconditionally.

--- target/riscv/cheri-archspecific-early.h
+++ target/riscv/cheri-archspecific-early.h
@@ -99,9 +99,9 @@ enum CheriSCR {
 #define CHERI_EXC_REGNUM_DDC (32 + CheriSCR_DDC)
 #define CHERI_CONTROLFLOW_CHECK_AT_TARGET 0
 #define CHERI_TAG_CLEAR_ON_INVALID(env) true
-#define CHERI_NO_RELOCATION(env)            (env_archcpu(env)->cfg.ext_cheri_v9)
-#define CHERI_TRANSLATE_DDC_RELOCATION(ctx) (!(ctx)->cheri_v9_semantics)
-#define CHERI_TRANSLATE_PCC_RELOCATION(ctx) (!(ctx)->cheri_v9_semantics)
+#define CHERI_NO_RELOCATION(env) true
+#define CHERI_TRANSLATE_DDC_RELOCATION(ctx) false
+#define CHERI_TRANSLATE_PCC_RELOCATION(ctx) false
 #define CINVOKE_DATA_REGNUM 31
 
 static inline const cap_register_t *cheri_get_ddc(CPURISCVState *env) {
