8fc2d848beeebe98bd1772324235de8619aab580
Martin Kaiser <martin.kaiser@codasip.com>
2024-07-02 16:10:33 +0200

    cheri: riscv: remove csetoffset
    Remove the csetoffset instruction from the risc-v cheri code. This
    instruction is no longer defined in the current risc-v cheri
    specification.
    
    As usual, we keep all code for this instruction that is shared
    between risc-v and other archtectures.

--- disas/riscv.c
+++ disas/riscv.c
@@ -565,7 +565,6 @@ typedef enum {
     rv_op_scbnds,
     rv_op_scbndsr,
     rv_op_acperm,
-    rv_op_csetoffset,
     rv_op_csub,
     rv_op_ctestsubset,
 
@@ -1335,7 +1334,6 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_scbnds] = { "scbnds", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
     [rv_op_scbndsr] = { "scbndsr", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
     [rv_op_acperm] = { "acperm", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
-    [rv_op_csetoffset] = { "csetoffset", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
     [rv_op_csub] = { "csub", rv_codec_r, rv_fmt_rd_cs1_cs2, NULL, 0, 0, 0 },
     [rv_op_ctestsubset] = { "ctestsubset", rv_codec_r, rv_fmt_rd_cs1_cs2, NULL, 0, 0, 0 },
 
@@ -1593,7 +1591,6 @@ static rv_opcode decode_cheri_inst(rv_inst inst) {
     CHERI_THREEOP_CASE(cspecialrw,  0000001,  ..... ..... 000 ..... 1011011 @r)
     // 0000010-0000111 unused
     // 0001010 unused
-    CHERI_THREEOP_CASE(csetoffset,  0001111,  ..... ..... 000 ..... 1011011 @r)
     CHERI_THREEOP_CASE(csub,        0010100,  ..... ..... 000 ..... 1011011 @r)
     // 0010101-0011101 unused
     CHERI_THREEOP_CASE(ctestsubset, 0100000,  ..... ..... 000 ..... 1011011 @r)
