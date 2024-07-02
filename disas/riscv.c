2cf8670fb72124daea0da64531a67ae251cb2f7b
Martin Kaiser <martin.kaiser@codasip.com>
2024-07-02 16:49:42 +0200

    cheri: riscv: remove cgetsealed
    Remove the cgetsealed instruction from the risc-v cheri code. This
    instruction is no longer defined in the current risc-v cheri
    specification.
    
    As usual, we keep all code for this instruction that is shared
    between risc-v and other archtectures.

--- disas/riscv.c
+++ disas/riscv.c
@@ -543,7 +543,6 @@ typedef enum {
     rv_op_csetboundsimm,
     rv_op_modesw,
     // Two operand
-    rv_op_cgetsealed,
     rv_op_cgetoffset,
     rv_op_crrl,
     rv_op_cram,
@@ -1297,7 +1296,6 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_csetboundsimm] = { "csetbounds", rv_codec_i, rv_fmt_cd_cs1_imm, NULL, 0, 0, 0 },
     [rv_op_modesw] = { "modesw", rv_codec_none, rv_fmt_none, NULL, 0, 0, 0 },
     // Two operand
-    [rv_op_cgetsealed] = { "cgetsealed", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_cgetoffset] = { "cgetoffset", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_crrl] = { "crrl", rv_codec_r, rv_fmt_rd_rs1, NULL, 0, 0, 0 },
     [rv_op_cram] = { "cram", rv_codec_r, rv_fmt_rd_rs1, NULL, 0, 0, 0 },
@@ -1564,7 +1562,6 @@ static const char *csr_name(int csrno)
 
 static rv_opcode decode_cheri_two_op(unsigned func) {
     switch (func) {
-    case 0b00101: return rv_op_cgetsealed;
     case 0b00110: return rv_op_cgetoffset;
     case 0b01000: return rv_op_crrl;
     case 0b01001: return rv_op_cram;
